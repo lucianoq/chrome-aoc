1cd2f4897a80c696f229f4051e38a9125219d99c
Luciano Quercia <luciano.quercia@gmail.com>
2019-08-05 21:37:08 +0100

    Version 0.3

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 htmlString = `
-    <div>
-        <h1 class="title-event">&nbsp;&nbsp;<span class="title-event-wrap">Luciano's</span>&nbsp;&nbsp;</h1>
-        <nav><ul><li><a id="yearInc" href="javascript:void(0);">[Year++]</a></li>
-            <li><a id="yearDec" href="javascript:void(0);">[Year--]</a></li>
-        </ul></nav>
-    </div>
+<div>
+    <h1 class="title-event">&nbsp;&nbsp;&nbsp;<span class="title-event-wrap">TimeTravel</span></h1>
+    <nav><ul><li><a id="yearInc" href="javascript:void(0);">[Year++]</a></li>
+        <li><a id="yearDec" href="javascript:void(0);">[Year--]</a></li>
+    </ul></nav>
+</div>
 `
 
 elem = $.parseHTML(htmlString);
@@ -12,30 +12,23 @@ $('header').append(elem);
 
 var moveYear = function (delta) {
     var url = window.location.href;
-    console.log(url);
 
-    var yearFields = url.match(/201./);
+    var re = /201./;
+
     var year = 2018;
+    var yearFields = url.match(re);
     if (yearFields != null) {
         year = parseInt(yearFields[0], 10);
     }
-    console.log(year);
-
 
     var newYear = year + delta;
-    console.log(newYear);
 
-    var re = new RegExp(year.toString());
-    console.log(re);
+    if (newYear < 2015 || newYear > 2018) {
+        return;
+    }
 
-    var newUrl = url.replace(re, newYear.toString());
-
-    console.log(newUrl);
-
-    window.location.href = newUrl;
+    window.location.href = url.replace(re, newYear.toString());;
 }
-
-console.log("adding listener");
 
 $('#yearInc').on("click", function () {
     moveYear(1);
